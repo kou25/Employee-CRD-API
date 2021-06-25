fed54a71152150af728c9ba66cfef9a3cf7521b2
Nitin Aggarwal <nitin@xenonstack.com>
2021-06-25 17:05:55 +0530

    add users listing

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ import Employees, { validateEmployee } from "./dbEmployee.js";
 const app = express();
 const port = process.env.PORT || 9000;
 
+import pg  from "pg";
+//creates the mysql database connection.
+const pool = new pg.Pool({
+    host     : "bizconnectaccountmanagement-dev.cnsnvikssz54.us-west-2.rds.amazonaws.com",
+    user     : "postgres",
+    password : "it1$raining",
+    database : "BizConnectAccountManagement",
+    port	   : "5432"
+});
+
+
 //middleware
 app.use(express.json());
 
@@ -68,6 +79,22 @@ app.post("/employee/new", async (req, res) => {
   });
   await employee.save();
   res.send(employee);
+});
+
+// api routes for post employee
+app.get("/users", async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');  //sets the contentype on the response header
+    pool.query("select first_name, last_name, full_name, email, company_name, bu.phone_number, country_name, bu.created_date from (select * from biz_user where active) as bu left join company c on c.id=bu.company_id order by bu.created_date desc;;", (err, result) => {
+        var list = [];
+        if (err) {
+          res.send(JSON.stringify({"status": 400, "error": err.stack, "response":list})); //return the json string
+          return
+        }
+        for (var i=0;i<result.rows.length;i++){
+            list.push(result.rows[i])
+        }
+        res.send(JSON.stringify({"status": 200, "error":false, "response": list})); //return the json string
+    })
 });
 
 //listen
